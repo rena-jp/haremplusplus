ac1d170f85dfb32b667b005e50522166f1978b87
liliat <liliat-hh@proton.me>
2022-12-06 15:49:54 +0100

    Replace mock scene image

--- src/mock/MockGameAPI.ts
+++ src/mock/MockGameAPI.ts
@@ -96,8 +96,8 @@ export class MockGameAPI implements GameAPI {
             questId: girl.quests[step].idQuest,
             dialogue:
               'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            scene: '/img/SalemGrade 1.jpg', // Scene images are blocked outside of the game; use a local img,
-            sceneFull: '/img/SalemGrade 1.jpg',
+            scene: '/img/scene.jpg', // Scene images are blocked outside of the game; use a local img,
+            sceneFull: '/img/scene.jpg',
             step: step
           });
         } else {
@@ -108,8 +108,8 @@ export class MockGameAPI implements GameAPI {
             dialogue:
               'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             portrait: girl.poseImage,
-            scene: '/img/SalemGrade 1.jpg', // Scene images are blocked outside of the game; use a local img,
-            sceneFull: '/img/SalemGrade 1.jpg',
+            scene: '/img/scene.jpg', // Scene images are blocked outside of the game; use a local img,
+            sceneFull: '/img/scene.jpg',
             step: step
           });
           this.fireRequestEvent('completed');
