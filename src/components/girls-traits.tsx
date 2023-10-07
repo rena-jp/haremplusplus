0f904f2b031c994457a87afe67405c331c95d343
rena-jp <101486573+rena-jp@users.noreply.github.com>
2023-10-07 20:53:50 +0900

    Change to display the second color as well

--- src/components/girls-traits.tsx
+++ src/components/girls-traits.tsx
@@ -12,30 +12,36 @@ export const GirlTraits = React.memo<GirlTraitsProps>(
   ({ girl, setSingleTrait }) => {
     const traitIcons = Traits.values().map((traitEnum) => {
       const blessing = Traits.toBlessing(traitEnum);
-      const values = Blessings.getBlessingValue(girl, blessing);
-      const value = Array.isArray(values) ? values[0] : values;
-      const trait: Trait = { traitEnum, traitValue: value };
+      const value = Blessings.getBlessingValue(girl, blessing);
+      const values = Array.isArray(value) ? value : [value];
 
       return (
-        <Tooltip
-          tooltip={
-            <span>
-              {Blessings.toDisplayType(blessing)}:
-              <br />
-              {Blessings.toDisplayString(blessing, value)}
-            </span>
-          }
-        >
-          <span
-            onClick={() => {
-              setSingleTrait(trait);
-            }}
-          >
-            <TraitIcon trait={trait} />
-          </span>
-        </Tooltip>
+        <span className="trait-group">
+          {values.map((value) => {
+            const trait: Trait = { traitEnum, traitValue: value };
+            return (
+              <Tooltip
+                tooltip={
+                  <span>
+                    {Blessings.toDisplayType(blessing)}:
+                    <br />
+                    {Blessings.toDisplayString(blessing, value)}
+                  </span>
+                }
+              >
+                <span
+                  onClick={() => {
+                    setSingleTrait(trait);
+                  }}
+                >
+                  <TraitIcon trait={trait} />
+                </span>
+              </Tooltip>
+            );
+          })}
+        </span>
       );
     });
-    return <div>{traitIcons}</div>;
+    return <div className="girl-traits">{traitIcons}</div>;
   }
 );
